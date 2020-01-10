6aaecc6e63f4b127b1674a13bdd864edb44ac7b4
Eugene Bednik <me@eugenebednik.com>
2020-01-09 23:43:55 -0800

    Additional commits

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const GoogleTranslate = require('./components/google-translate/GoogleTranslate.js');
+const languages = require('./resources/languages.js');
+const { flag, code, name, countries } = require('country-emoji');
 
 const client = new Discord.Client();
 const prefix = config.prefix;
-const googleTranslate = new GoogleTranslate(config.google.translate.apiKey);
+const googleTranslate = new GoogleTranslate(config.google.translate.apiKey, languages);
 const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
 client.once('ready', () => {
     console.log('Ready!');
+});
+
+client.on('messageReactionAdd', (messageReaction, user) => {
+    if (user.bot) return;
+    const { message, emoji } = messageReaction;
+
+    let countryCode = code(emoji.name);
+    if (countryCode !== undefined) {
+        googleTranslate.translate(countryCode.toLowerCase(), message.content, message, false);
+    }
 });
 
 client.on('message', message => {
@@ -21,24 +33,17 @@ client.on('message', message => {
 
     switch (command) {
         case 'ping':
-            {
-                console.log('here');
-                message.channel.send('pong');
-                break;
-            }
+            console.log('here');
+            message.channel.send('pong');
+            break;
         case 'tr':
-            {
-                if (args.length < 2) return;
-                const targetLang = args.shift().toLowerCase();
-                const textToTranslate = args.join(' ');
-                googleTranslate.translate(targetLang, textToTranslate, message);
-                break;
-            }
+            if (args.length < 2) return;
+            googleTranslate.translate(args.shift().toLowerCase(), args.join(' '), message, true);
+            break;
         case 'prefix':
-            {
-                message.reply(`You can either ping me or use \`${prefix}\` as my prefix.`);
-            }
+            message.reply(`You can either ping me or use \`${prefix}\` as my prefix.`);
     }
+
 });
 
 client.login(config.token);

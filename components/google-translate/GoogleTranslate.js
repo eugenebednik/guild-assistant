2f1f30a04324a2e669154dd6c25cd45067d2d8b1
Eugene Bednik <eugene.bednik@leafgroup.com>
2020-02-08 12:57:54 -0800

    More cleanup

--- components/google-translate/GoogleTranslate.js
+++ components/google-translate/GoogleTranslate.js
@@ -2,73 +2,74 @@ const countryCode = require('country-language');
 const languageDefinitions = require('../../resources/languages');
 
 class GoogleTranslate {
-    constructor(apiKey, logger) {
-        this.googleTranslate = require('google-translate')(apiKey);
-        this.logger = logger;
-    }
+  constructor(apiKey, logger) {
+    this.googleTranslate = require('google-translate')(apiKey);
+    this.logger = logger;
+  }
 
-    translate(targetLang, textToTranslate, message, isCommand = false) {
-        let targetLanguage = null;
+  translate(targetLang, textToTranslate, message, isCommand = false) {
+    let targetLanguage;
 
-        if (!isCommand) {
-            const countryLang = countryCode.getCountryLanguages(targetLang.toUpperCase(), (err, languages) => {
-                if (err) throw err;
-                if (languages && languages[0]) {
-                    if (targetLang === 'il') {
-                        return 'he';
-                    }
-                    else {
-                        return languages[0].iso639_1.toLowerCase();
-                    }
-                }
-                else {
-                    return 'en';
-                }
-            });
-
-            targetLanguage = languageDefinitions.find(lang => lang.code === countryLang);
+    if (!isCommand) {
+      const countryLang = countryCode.getCountryLanguages(targetLang.toUpperCase(), (err, languages) => {
+        if (err) throw err;
+        if (languages && languages[0]) {
+          if (targetLang === 'il') {
+            return 'he';
+          }
+          else {
+            return languages[0].iso639_1.toLowerCase();
+          }
         }
         else {
-            targetLanguage = languageDefinitions.find(lang => lang.code === targetLang.toLowerCase());
-
-            if (!targetLanguage) {
-                targetLanguage = languageDefinitions.find(lang => lang.code === 'en');
-            }
+          return 'en';
         }
+      });
 
-        this.googleTranslate.translate(textToTranslate, targetLanguage.code, (err, results) => {
-            if (err) {
-                this.logger.error('ERROR:', err);
-                throw err;
-            }
-
-            if (results) {
-                const translatedText = results.translatedText.trim();
-                const sourceLanguage = languageDefinitions.find(lang => lang.code === results.detectedSourceLanguage.toLowerCase());
-
-                if (isCommand) {
-                    message.reply(targetLanguage.name + ':\n' + '```' + translatedText + '```');
-                }
-                else {
-                    message.channel.send({
-                        embed: {
-                            color: 3447003,
-                            author: {
-                                name: message.author.username,
-                                icon_url: message.author.avatarURL,
-                            },
-
-                            description: translatedText,
-
-                            footer: {
-                                text: sourceLanguage.name + ' ⟶ ' + targetLanguage.name,
-                            },
-                        },
-                    });
-                }
-            }
-        });
+      targetLanguage = languageDefinitions.find(lang => lang.code === countryLang);
     }
+    else {
+      targetLanguage = languageDefinitions.find(lang => lang.code === targetLang.toLowerCase());
+
+      if (!targetLanguage) {
+        targetLanguage = languageDefinitions.find(lang => lang.code === 'en');
+      }
+    }
+
+    this.googleTranslate.translate(textToTranslate, targetLanguage.code, (err, results) => {
+      if (err) {
+        this.logger.error('ERROR:', err);
+        throw err;
+      }
+
+      if (results) {
+        const translatedText = results.translatedText.trim();
+        const sourceLanguage = languageDefinitions.find(lang =>
+          lang.code === results.detectedSourceLanguage.toLowerCase());
+
+        if (isCommand) {
+          message.reply(targetLanguage.name + ':\n' + '```' + translatedText + '```');
+        }
+        else {
+          message.channel.send({
+            embed: {
+              color: 3447003,
+              author: {
+                name: message.author.username,
+                icon_url: message.author.avatarURL,
+              },
+
+              description: translatedText,
+
+              footer: {
+                text: sourceLanguage.name + ' ⟶ ' + targetLanguage.name,
+              },
+            },
+          });
+        }
+      }
+    });
+  }
 }
 
 module.exports = GoogleTranslate;
--- index.js
+++ index.js
@@ -11,35 +11,34 @@ const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 // Logging
 const log4js = require('log4js');
 log4js.configure({
-    appenders: {
-        botLogger: { type: 'file', filename: process.env.LOG_FILE },
-    },
-    categories: {
-        default: { appenders: ['botLogger'], level: process.env.LOG_LEVEL },
-    },
+  appenders: {
+    botLogger: { type: 'file', filename: process.env.LOG_FILE },
+  },
+  categories: {
+    default: { appenders: ['botLogger'], level: process.env.LOG_LEVEL },
+  },
 });
 
 const logger = log4js.getLogger('botLogger');
 
 // Database stuff
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 // connect to database
 db.connect(err => {
-    if (err) {
-        logger.error('ERROR:', err);
-        throw err;
-    }
-    console.log('Connected to the database.');
+  if (err) {
+    logger.error('ERROR:', err);
+    throw err;
+  }
+  console.log('Connected to the database.');
 });
 
 // Globals
-let global;
 global.db = db;
 
 // Helpers
@@ -56,231 +55,234 @@ const { code } = require('country-emoji');
 const unirest = require('unirest');
 
 client.on('guildCreate', guild => {
-    console.log(`Joined a new guild: ${guild.name}`);
+  console.log(`Joined a new guild: ${guild.name}`);
 });
 
 // App begin
 client.once('ready', () => {
-    console.log('Ready!');
+  console.log('Ready!');
 });
 
 client.on('messageReactionAdd', (messageReaction, user) => {
-    if (user.bot) return;
-    const { message, emoji } = messageReaction;
+  if (user.bot) return;
+  const { message, emoji } = messageReaction;
 
-    if (message.author.bot) return;
+  if (message.author.bot) return;
 
-    const countryCode = code(emoji.name);
-    if (countryCode) {
-        const sanitizedMessage = replaceMentions(client, message.content, message.guild);
-        googleTranslate.translate(countryCode.toLowerCase(), sanitizedMessage, message, false);
-    }
+  const countryCode = code(emoji.name);
+  if (countryCode) {
+    const sanitizedMessage = replaceMentions(client, message.content, message.guild);
+    googleTranslate.translate(countryCode.toLowerCase(), sanitizedMessage, message, false);
+  }
 });
 
 client.on('message', message => {
-    getGuildId(message, guildId => {
-        const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
-        if (!prefixRegex.test(message.content)) return;
+  getGuildId(message, guildId => {
+    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
+    if (!prefixRegex.test(message.content)) return;
 
-        let targetLang = args.shift().toLowerCase();
+    const [, matchedPrefix] = message.content.match(prefixRegex);
+    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
 
-        const [, matchedPrefix] = message.content.match(prefixRegex);
-        const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-        const sanitizedMessage = replaceMentions(client, args.join(' '), message.guild);
-        const targetLangEmoji = code(targetLang);
+    let sanitizedMessage;
+    let targetLang;
+    let targetLangEmoji;
+    let subCommand;
+    let sql;
+    let serverGmtOffset;
 
-        let subCommand = null;
-        let serverGmtOffset = null;
-        let sql = '';
+    switch (command) {
+    case 'ping':
+      message.channel.send('pong');
+      break;
+    case 'beep':
+      message.channel.send('boop');
+      break;
+    case 'stats':
+      if (!message.member.hasPermission('ADMINISTRATOR')) {
+        message.reply(i18n.general.accessDenied);
+        return;
+      }
 
-        switch (command) {
-        case 'ping':
-            message.channel.send('pong');
-            break;
-        case 'beep':
-            message.channel.send('boop');
-            break;
-        case 'stats':
-            if (!message.member.hasPermission('ADMINISTRATOR')) {
-                message.reply(i18n.general.accessDenied);
+      message.channel.send(`Server count: ${client.guilds.size}`);
+      break;
+    case 't':
+      if (args.length < 2 || (args.length === 1 && args[0].toLowerCase() === 'help')) {
+        message.reply(i18n.commands.t.syntaxHelp);
+
+        return;
+      }
+
+      targetLang = args.shift().toLowerCase();
+      sanitizedMessage = replaceMentions(client, args.join(' '), message.guild);
+      targetLangEmoji = code(targetLang);
+
+      if (targetLangEmoji) {
+        targetLang = targetLangEmoji.toLowerCase();
+      }
+
+      googleTranslate.translate(targetLang, sanitizedMessage, message, true);
+      break;
+    case 'prefix':
+      message.reply(i18n.commands.ping.message + `\`${prefix}\``);
+      break;
+    case 'servertime':
+      if (args.length) {
+        subCommand = args.shift().toLowerCase();
+      }
+
+      if (subCommand) {
+        let offsetTz = 'GMT';
+        const param = args[0].toLowerCase();
+
+        switch (subCommand) {
+        case 'help':
+          message.reply(i18n.commands.servertime.syntaxHelp);
+          return;
+        case 'offset':
+          if (!args.length) {
+            message.reply(i18n.commands.servertime.incorrectOffset);
+            return;
+          }
+
+          if (!message.member.hasPermission('ADMINISTRATOR')) {
+            message.reply(i18n.general.accessDenied);
+            return;
+          }
+
+          if (param !== 'gmt') {
+            const match = param.match(/[-+]\d{1,2}/);
+
+            if (match) {
+              const sign = match[0].substr(0, 1);
+              const number = match[0].substr(1, match[0].length);
+
+              if (sign === '+' && (number < 1 || number > 14)) {
+                message.reply(i18n.commands.servertime.incorrectOffset);
                 return;
-            }
+              }
 
-            message.channel.send(`Server count: ${client.guilds.size}`);
-            break;
-        case 't':
-            if (args.length < 2 || (args.length === 1 && args[0].toLowerCase() === 'help')) {
-                message.reply(i18n.commands.t.syntaxHelp);
-
+              if (sign === '-' && (number < 1 || number > 9)) {
+                message.reply(i18n.commands.servertime.incorrectOffset);
                 return;
-            }
+              }
 
-            if (targetLangEmoji) {
-                targetLang = targetLangEmoji.toLowerCase();
-            }
-
-            googleTranslate.translate(targetLang, sanitizedMessage, message, true);
-            break;
-        case 'prefix':
-            message.reply(i18n.commands.ping.message + `\`${prefix}\``);
-            break;
-        case 'servertime':
-            if (args.length) {
-                subCommand = args.shift().toLowerCase();
-            }
-
-            if (subCommand) {
-                let offsetTz = 'GMT';
-                const param = args[0].toLowerCase();
-
-                switch (subCommand) {
-                case 'help':
-                    message.reply(i18n.commands.servertime.syntaxHelp);
-                    return;
-                case 'offset':
-                    if (!args.length) {
-                        message.reply(i18n.commands.servertime.incorrectOffset);
-                        return;
-                    }
-
-                    if (!message.member.hasPermission('ADMINISTRATOR')) {
-                        message.reply(i18n.general.accessDenied);
-                        return;
-                    }
-
-                    if (param !== 'gmt') {
-                        const match = param.match(/[-+]\d{1,2}/);
-
-                        if (match) {
-                            const sign = match[0].substr(0, 1);
-                            const number = match[0].substr(1, match[0].length);
-
-                            if (sign === '+' && (number < 1 || number > 14)) {
-                                message.reply(i18n.commands.servertime.incorrectOffset);
-                                return;
-                            }
-
-                            if (sign === '-' && (number < 1 || number > 9)) {
-                                message.reply(i18n.commands.servertime.incorrectOffset);
-                                return;
-                            }
-
-                            offsetTz += sign + number;
-                        }
-                        else {
-                            message.reply(i18n.commands.servertime.incorrectOffset);
-                            return;
-                        }
-                    }
-
-                    sql = `REPLACE INTO \`guild_gmt_offsets\` (offset, guild_id) VALUES ('${offsetTz.toUpperCase()}', ${guildId});`;
-
-                    db.query(sql, (err, result) => {
-                        if (err) {
-                            logger.error('ERROR:', err);
-                            throw err;
-                        }
-
-                        if (result) {
-                            message.reply(i18n.commands.servertime.offsetSetSuccess + `\`${offsetTz}\``);
-                        }
-                        else {
-                            message.reply(i18n.commands.servertime.offsetSetFailure);
-                        }
-                    });
-                    break;
-                default:
-                    message.reply(i18n.commands.servertime.syntaxHelp);
-                    return;
-                }
-            }
-
-            sql = `SELECT offset FROM \`guild_gmt_offsets\` WHERE guild_id = ${guildId} LIMIT 1;`;
-
-            db.query(sql, (err, result) => {
-                if (err) {
-                    logger.error('ERROR:', err);
-                    throw err;
-                }
-
-                if (!result.length) {
-                    message.reply(i18n.commands.servertime.firstRunHelp);
-                    return;
-                }
-
-                serverGmtOffset = result[0].offset;
-
-                try {
-                    unirest.get(`${process.env.TIMEZONE_API_URL}/${serverGmtOffset}`).end(response => {
-                        if (response.ok) {
-                            const dateTime = moment.parseZone(response.body.datetime).format('dddd, MMMM Do YYYY, HH:mm:ss');
-
-                            message.channel.send({
-                                embed: {
-                                    color: 3447003,
-                                    author: {
-                                        name: message.author.username,
-                                        icon_url: message.author.avatarURL,
-                                    },
-                                    description: `${i18n.commands.servertime.serverTime}: ${dateTime}`,
-
-                                    footer: {
-                                        text: `${i18n.commands.servertime.serverTime}: ${serverGmtOffset}`,
-                                    },
-                                },
-                            });
-                        }
-                    });
-                }
-                catch (err) {
-                    logger.error('ERROR:', err);
-                    throw err;
-                }
-            });
-
-            break;
-        case 'msgmove':
-            if (!args.length || (args.length && args.length !== 2)) {
-                message.reply(i18n.commands.msgmove.syntaxHelp);
-                return;
+              offsetTz += sign + number;
             }
             else {
-                moveMessage(args, client, message);
+              message.reply(i18n.commands.servertime.incorrectOffset);
+              return;
+            }
+          }
+
+          sql = `REPLACE INTO \`guild_gmt_offsets\` (offset, guild_id) VALUES ('${offsetTz.toUpperCase()}', ${guildId});`;
+
+          db.query(sql, (err, result) => {
+            if (err) {
+              logger.error('ERROR:', err);
+              throw err;
             }
 
-            break;
+            if (result) {
+              message.reply(i18n.commands.servertime.offsetSetSuccess + `\`${offsetTz}\``);
+            }
+            else {
+              message.reply(i18n.commands.servertime.offsetSetFailure);
+            }
+          });
+          break;
         default:
-            break;
+          message.reply(i18n.commands.servertime.syntaxHelp);
+          return;
         }
-    });
-});
+      }
 
-function getGuildId(message, callback) {
-    let sql = `SELECT id FROM guilds WHERE identifier = '${message.guild.id}' LIMIT 1;`;
+      sql = `SELECT offset FROM \`guild_gmt_offsets\` WHERE guild_id = ${guildId} LIMIT 1;`;
 
-    db.query(sql, (err, result) => {
+      db.query(sql, (err, result) => {
         if (err) {
-            logger.error('ERROR:', err);
-            throw err;
+          logger.error('ERROR:', err);
+          throw err;
         }
 
         if (!result.length) {
-            sql = `REPLACE INTO \`guilds\` (identifier, name) VALUES ('${message.guild.id}', '${message.guild.name}');`;
-
-            db.query(sql, (err) => {
-                if (err) {
-                    logger.error('ERROR:', err);
-                    throw err;
-                }
-
-                callback(result.insertId);
-            });
+          message.reply(i18n.commands.servertime.firstRunHelp);
+          return;
         }
-        else {
-            callback(result[0].id);
+
+        serverGmtOffset = result[0].offset;
+
+        try {
+          unirest.get(`${process.env.TIMEZONE_API_URL}/${serverGmtOffset}`).end(response => {
+            if (response.ok) {
+              const dateTime = moment.parseZone(response.body.datetime).format('dddd, MMMM Do YYYY, HH:mm:ss');
+
+              message.channel.send({
+                embed: {
+                  color: 3447003,
+                  author: {
+                    name: message.author.username,
+                    icon_url: message.author.avatarURL,
+                  },
+                  description: `${i18n.commands.servertime.serverTime}: ${dateTime}`,
+
+                  footer: {
+                    text: `${i18n.commands.servertime.serverTime}: ${serverGmtOffset}`,
+                  },
+                },
+              });
+            }
+          });
         }
-    });
+        catch (err) {
+          logger.error('ERROR:', err);
+          throw err;
+        }
+      });
+
+      break;
+    case 'msgmove':
+      if (!args.length || (args.length && args.length !== 2)) {
+        message.reply(i18n.commands.msgmove.syntaxHelp);
+        return;
+      }
+      else {
+        moveMessage(args, client, message);
+      }
+
+      break;
+    default:
+      break;
+    }
+  });
+});
+
+function getGuildId(message, callback) {
+  let sql = `SELECT id FROM guilds WHERE identifier = '${message.guild.id}' LIMIT 1;`;
+
+  db.query(sql, (err, result) => {
+    if (err) {
+      logger.error('ERROR:', err);
+      throw err;
+    }
+
+    if (!result.length) {
+      sql = `REPLACE INTO \`guilds\` (identifier, name) VALUES ('${message.guild.id}', '${message.guild.name}');`;
+
+      db.query(sql, (err) => {
+        if (err) {
+          logger.error('ERROR:', err);
+          throw err;
+        }
+
+        callback(result.insertId);
+      });
+    }
+    else {
+      callback(result[0].id);
+    }
+  });
 }
 
 client.login(process.env.BOT_DISCORD_TOKEN);
